4692704601b763aa35e3789e6978348a5bdb1c20
Siajiza <>
2021-04-06 09:21:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+sentence = 'The quick brown fox jumped over the lazy dog.'
+print(sentence.replace('quick', 'slow'))
+
+#--------------------------------------
+
+sentence = sentence.replace('quick', 'slow')
+print(sentence)
